1e4af2976ea65a345c8a043a1c910b972ab07183
Barbara Pascal <pascalba92@gmail.com>
2023-07-19 22:22:59 +0200

    homogeneous fgn

---
+++ synthesis/fgn1D.m
@@ -0,0 +1,52 @@
+
+function U=fgn1D(N,H,Variance)
+    
+    
+    % Generate piecewise homogeneous fractal process with prescribed
+    % local regularity and local variance from a generalized model of
+    % Fractional Gaussian Noise combining horizontal and vertical 
+    % increments of the Fractional Brownian Motion
+    %
+    % from
+    % - B. Pascal, N. Pustelnik, P. Abry: Strongly Convex Optimization for 
+    % Joint Fractal Feature Estimation and Texture Segmentation, 
+    % (2019) arxiv:1910.05246
+    % 
+    % Makes use of the implementation of FBM by M. Clausel and B. Vedel, 
+    % 2008, updated by S. Roux, 2011
+    
+    %% Modified by B. Pascal
+    
+    % July, 18th 2018
+    l1=1;l2=1;M=N-1;
+    
+    %COORDINATES
+    X=(-2*2^M:2:2*2^M)/(2^(M+1));
+    X(2^M+1)=1/2^M;
+    
+    %NOUVELLE DENSITE SPECTRALE PSI
+    tau=abs(X);
+    phi=tau.^(H+(l1)/2);
+
+    
+    
+    
+    %CONSTRUCTION OF THE FOURIER TRANSFORM OF THE FIELD (WITHOUT RENORMALIZATION)
+    %W= Fourier transform of the OSSRGF
+    %Zr and Zi avoid to include additional symmetries in Fourier
+    Zr=randn(1,2*2^M+1);
+    Zi=randn(1,2*2^M+1);
+    Z = Zr + 1i*Zi;
+    
+    W=fftshift(fft2(Z))./phi;
+    
+    
+    %CONSTRUCTION OF THE FIELD (FOURIER INVERSE + RENORMALIZATION)
+    T  = real(ifft2(ifftshift(W)));
+    Zp = T-T(2^M+1);
+    Zp = Zp(2:end)-Zp(1:end-1);
+    Zp = Zp/std(Zp);
+
+    U = sqrt(Variance).*Zp;
+    
+end
